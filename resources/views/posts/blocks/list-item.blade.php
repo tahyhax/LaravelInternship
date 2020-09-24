e9dda9ec9cf9df61401929308c9e551ee6319746
tahyhax <dimon4i4ek@ukr.net>
2020-09-24 19:23:29 +0300

    Lesson-1 add  post info

--- resources/views/posts/blocks/list-item.blade.php
+++ resources/views/posts/blocks/list-item.blade.php
@@ -1,11 +1,18 @@
 <div class="card mb-3">
     <div class="card-body">
-        <h5 class="card-title"><a href="{{route('posts.show', ['post' => $post->id])}}">{{$post->title}}</a></h5>
-        <p class="card-text">{{$post->body}}</p>
-        <p class="card-text">
-            <small class="text-muted">Last updated 3 mins ago</small>
-        </p>
 
+        <h5 class="card-title mb-1"><a href="{{route('posts.show', ['post' => $post->id])}}">{{$post->title}}</a></h5>
+        <div class="mb-1">
+            Creator: <small class="text-muted">{{$post->user->name}}</small>
+            Created: <small class="text-muted">{{ $post->created_at }}</small>
+        </div>
+        <p class="card-text">{{$post->body}}</p>
+        {{--<p class="card-text">--}}
+            {{--<small class="text-muted">Last updated 3 mins ago</small>--}}
+        {{--</p>--}}
+
+    </div>
+    <div class="card-footer">
         <div class="button-group d-flex justify-content-end">
             <a class="btn btn-primary mr-1" href="{{route('posts.edit', ['post' => $post->id])}}" role="button">Edit</a>
             <form method="POST" action="{{route('posts.destroy', ['post' => $post->id])}}">
@@ -15,4 +22,5 @@
             </form>
         </div>
     </div>
+
 </div>
