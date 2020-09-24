95598fa8b7ba799914e8cd920101ba8a4bad4a8f
tahyhax <dimon4i4ek@ukr.net>
2020-09-24 19:32:37 +0300

    Lesson-1 add  add created at  format

--- resources/views/posts/blocks/list-item.blade.php
+++ resources/views/posts/blocks/list-item.blade.php
@@ -4,13 +4,13 @@
         <h5 class="card-title mb-1"><a href="{{route('posts.show', ['post' => $post->id])}}">{{$post->title}}</a></h5>
         <div class="mb-1">
             Creator: <small class="text-muted">{{$post->user->name}}</small>
-            Created: <small class="text-muted">{{ $post->created_at }}</small>
+            {{--// NOTE временно сделано пока не разберуть почему на работает $casts--}}
+            Created: <small class="text-muted">{{ $post->created_at->isoFormat('DD/MM/Y hh:m') }}</small>
+            @if($post->created_at != $post->updated_at)
+                Updated: <small class="text-muted">{{ $post->updated_at->isoFormat('DD/MM/Y hh:mm') }}</small>
+            @endif
         </div>
         <p class="card-text">{{$post->body}}</p>
-        {{--<p class="card-text">--}}
-            {{--<small class="text-muted">Last updated 3 mins ago</small>--}}
-        {{--</p>--}}
-
     </div>
     <div class="card-footer">
         <div class="button-group d-flex justify-content-end">
