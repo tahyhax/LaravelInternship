a86893f17b60fe1de1ca241bc2892ff7bec9aaa1
tahyhax <dimon4i4ek@ukr.net>
2020-09-30 14:18:28 +0300

    Lesson-1 add auth permittions and diffForHumans

--- resources/views/posts/blocks/list-item.blade.php
+++ resources/views/posts/blocks/list-item.blade.php
@@ -4,23 +4,26 @@
         <h5 class="card-title mb-1"><a href="{{route('posts.show', ['post' => $post->id])}}">{{$post->title}}</a></h5>
         <div class="mb-1">
             Creator: <small class="text-muted">{{$post->user->name}}</small>
-            {{--// NOTE временно сделано пока не разберуть почему на работает $casts--}}
-            Created: <small class="text-muted">{{ $post->created_at->isoFormat('DD/MM/Y hh:m') }}</small>
+            {{--// NOTE временно сделано пока не разберуcь почему на работает $casts--}}
+            Created: <small class="text-muted">{{ $post->created_at->diffForHumans() }}</small>
             @if($post->created_at != $post->updated_at)
-                Updated: <small class="text-muted">{{ $post->updated_at->isoFormat('DD/MM/Y hh:mm') }}</small>
+                Updated: <small class="text-muted">{{ $post->updated_at->diffForHumans() }}</small>
             @endif
         </div>
         <p class="card-text">{!! $post->body !!}</p>
     </div>
-    <div class="card-footer">
-        <div class="button-group d-flex justify-content-end">
-            <form method="POST" action="{{route('posts.destroy', ['post' => $post->id])}}">
-                @csrf
-                @method('DELETE')
-                <a class="btn btn-primary mr-1" href="{{route('posts.edit', ['post' => $post->id])}}" role="button">Edit</a>
-                <button  type="submit" class="btn btn-danger" onclick="return confirm('Sure Want Delete?')">Delete</button>
-            </form>
+    @auth
+        <div class="card-footer">
+            <div class="button-group d-flex justify-content-end">
+                <form method="POST" action="{{route('posts.destroy', ['post' => $post->id])}}">
+                    @csrf
+                    @method('DELETE')
+                    <a class="btn btn-primary mr-1" href="{{route('posts.edit', ['post' => $post->id])}}" role="button">Edit</a>
+                    <button  type="submit" class="btn btn-danger" onclick="return confirm('Sure Want Delete?')">Delete</button>
+                </form>
+            </div>
         </div>
-    </div>
+    @endauth
+
 
 </div>
