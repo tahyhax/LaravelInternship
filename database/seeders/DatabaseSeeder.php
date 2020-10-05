27338a990e5a1cb12d2cc2b245ebdf2418192800
tahyhax <dimon4i4ek@ukr.net>
2020-10-05 19:04:57 +0300

    Lesson-2 add  run all fakers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Post;
-use App\Models\Comment;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
-use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         User::factory(10)->hasPosts(5)->count(5)->create();
+        Brand::factory()->count(10)->create();
+        Category::factory()->count(3)->create();
+        Product::factory()->count(50)
+            ->has(
+                Category::factory(), 'categories'
+            )
+            ->for(
+                Brand::factory(), 'brand'
+            )
+//            ->for(Brand::factory(), 'brand')
+            ->create();
+
+//         User::factory(10)->hasPosts(5)->count(5)->create();
 //         Post::factory(50)->create();
+
     }
 }
