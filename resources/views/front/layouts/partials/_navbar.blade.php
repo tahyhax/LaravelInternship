35a670b5a8ec30ab1e21d3d5a1298d4500917af4
tahyhax <dimon4i4ek@ukr.net>
2020-10-07 13:45:53 +0300

    Lesson-2  remove  space in file

--- resources/views/front/layouts/partials/_navbar.blade.php
+++ resources/views/front/layouts/partials/_navbar.blade.php
@@ -3,13 +3,13 @@
         @guest
             <a class="btn btn-outline-primary" href="{{ route('login') }}">Sign in</a>
         @else
-
             <div class="btn-group">
-                <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
                     {{ Auth::user()->name }}
                 </button>
                 <div class="dropdown-menu dropdown-menu-right">
-                    <a class="dropdown-item" href="{{ route('logout') }}"  onclick="event.preventDefault();
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
                         Logout
                     </a>
