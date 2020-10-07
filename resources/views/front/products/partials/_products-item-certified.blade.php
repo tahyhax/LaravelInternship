995fca202574ffa4f1ba05a4bbac67b7c16bbf8b
tahyhax <dimon4i4ek@ukr.net>
2020-10-07 23:26:11 +0300

    Lesson-2  add product view

---
+++ resources/views/front/products/partials/_products-item-certified.blade.php
@@ -0,0 +1,10 @@
+<div class="certified">
+    <ul>
+        <li>
+            <a href="javascript:void(0);">Delivery time<span>7 Working Days</span></a>
+        </li>
+        <li>
+            <a href="javascript:void(0);">Certified<span>Quality Assured</span></a>
+        </li>
+    </ul>
+</div>
