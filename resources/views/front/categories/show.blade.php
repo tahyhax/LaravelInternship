c662848f88fb400de1074ae2a2865246e7af3b13
tahyhax <dimon4i4ek@ukr.net>
2020-10-07 13:46:16 +0300

    Lesson-2  add show  category view

---
+++ resources/views/front/categories/show.blade.php
@@ -0,0 +1,27 @@
+@extends('front.layouts.app')
+
+@section('title', $category->name)
+
+@section('sidebar-left')
+    <div class="col-lg-3">
+        @include('front.layouts.partials.sidebar._categories')
+    </div>
+@endsection
+@section('content')
+    <div class="col-lg-9">
+        <div class="mb-4">
+            @include('front.main.partials._slider')
+        </div>
+
+        <div class="row">
+            @foreach($category->products as $product)
+                <div class="col-lg-4 col-md-6 mb-4">
+                    @include('front.main.partials._products-list-item', [
+                    'category' => $category,
+                    'product' => $product
+                    ])
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
