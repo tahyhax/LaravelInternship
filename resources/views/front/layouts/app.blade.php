cc43d6456745cef6c2d3ea6c448552eaf38bab12
tahyhax <dimon4i4ek@ukr.net>
2020-10-07 23:27:05 +0300

    Lesson-2  add  stack for  styles

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Document')</title>
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    @stack('styles')
 </head>
 <body>
 
