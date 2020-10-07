12735b703e7b564167c41412dc73bec24b9d8fd3
tahyhax <dimon4i4ek@ukr.net>
2020-10-07 13:06:47 +0300

    Lesson-2  create  - view  service provider  - view composer sidebar categories

---
+++ app/Http/View/Composers/SidebarCategoriesComposer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\View\Composers;
+
+use App\Models\Category;
+use Illuminate\View\View;
+
+class SidebarCategoriesComposer
+{
+    /**
+     * The category model.
+     *
+     * @var $category
+     */
+    protected $category;
+
+    /**
+     * Create a new profile composer.
+     *
+     * @param  Category $category
+     * @return void
+     */
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $view->with('categories', $this->category->all());
+    }
+}
