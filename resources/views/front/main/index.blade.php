f10176e4c6436be43f3f54839298b5962fec2cb6
tahyhax <dimon4i4ek@ukr.net>
2020-10-08 14:18:13 +0300

    Lesson-2 use  product-card component

--- resources/views/front/main/index.blade.php
+++ resources/views/front/main/index.blade.php
@@ -17,7 +17,7 @@
         <div class="row">
             @foreach($products as $product)
                 <div class="col-lg-4 col-md-6 mb-4">
-                    @include('front.main.partials._products-list-item', ['product' => $product])
+                    <x-the-product-card :product="$product"></x-the-product-card>
                 </div>
             @endforeach
         </div>
