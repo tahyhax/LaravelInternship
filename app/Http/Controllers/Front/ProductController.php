f8b895ad48723a53ecda18e7e1b6b0c231b3b423
tahyhax <dimon4i4ek@ukr.net>
2020-10-09 15:15:14 +0300

    Lesson-2 remove not used actions

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -18,27 +18,6 @@ class ProductController extends Controller
 //    {
 //        //
 //    }
-//
-//    /**
-//     * Show the form for creating a new resource.
-//     *
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function create()
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Store a newly created resource in storage.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function store(Request $request)
-//    {
-//        //
-//    }
 
     /**
      * Display the specified resource.
@@ -49,42 +28,8 @@ class ProductController extends Controller
     public function show(Product $product)
     {
         $product->load('brand');
-        return view('front.products.index')->with(
+        return view('front.products.show')->with(
             compact(['product'])
         );
     }
-
-//    /**
-//     * Show the form for editing the specified resource.
-//     *
-//     * @param  \App\Models\Product  $product
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function edit(Product $product)
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Update the specified resource in storage.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @param  \App\Models\Product  $product
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function update(Request $request, Product $product)
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Remove the specified resource from storage.
-//     *
-//     * @param  \App\Models\Product  $product
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function destroy(Product $product)
-//    {
-//        //
-//    }
 }
