8d49d1c6b6317c83bb68e8c4eccba748bb3663a3
tahyhax <dimon4i4ek@ukr.net>
2020-10-11 21:35:36 +0300

    Lesson-2 call seeder  by model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,20 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Brand::factory()->count(10)->create();
-        Category::factory()->count(3)->create();
-        Product::factory()->count(50)
-            ->has(
-                Category::factory(), 'categories'
-            )
-            ->for(
-                Brand::factory(), 'brand'
-            )
-//            ->for(Brand::factory(), 'brand')
-            ->create();
 
-//         User::factory(10)->hasPosts(5)->count(5)->create();
-//         Post::factory(50)->create();
+        $this->call([
+//            RoleSeeder::class,
+//            UserSeeder::class,
+            BrandSeeder::class,
+            CategorySeeder::class
+        ]);
 
     }
 }
