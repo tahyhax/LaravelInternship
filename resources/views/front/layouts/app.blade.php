eb097d92cac8d15490b4e1324d0ecf427c105dd9
tahyhax <dimon4i4ek@ukr.net>
2020-10-14 20:50:32 +0300

    Lesson-2  include  new  separated  styles

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Document')</title>
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <link rel="stylesheet" href="{{mix('css/front/app.css')}}">
     @stack('styles')
 </head>
 <body>
@@ -24,7 +24,7 @@
 
 </div>
 
-<script src="{{mix('js/app.js')}}"></script>
+<script src="{{mix('js/front/app.js')}}"></script>
 
 </body>
 </html>
