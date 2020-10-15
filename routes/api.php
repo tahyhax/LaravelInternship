3293778dae9d6d1bf4a264551ce0dcf1a2f49b87
tahyhax <dimon4i4ek@ukr.net>
2020-10-15 16:34:12 +0300

    Lesson-2   add router apiResource to category

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// TODO 'middleware' => ['auth:api'] говорит что нет auth token
+Route::group(['as' => 'api.','namespace' => '\App\Http\Controllers\Api'], function () {
+    Route::apiResource('categories',    CategoryController::class);
+});
