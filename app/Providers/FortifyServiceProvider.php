ec7d30a298b2569c6f0632f15d4ee34ae3830f06
tahyhax <dimon4i4ek@ukr.net>
2020-10-15 16:35:44 +0300

    Lesson-2  fix  typo reset  passord

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -41,10 +41,13 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.register');
         });
         Fortify::requestPasswordResetLinkView(function () {
-            return view('auth.forgot-password');
+            return view('auth.passwords.forgot');
         });
         Fortify::resetPasswordView(function () {
             return view('auth.passwords.reset');
         });
+//        Fortify::resetPasswordView(function ($request) {
+//            return view('auth.reset-password', ['request' => $request]);
+//        });
     }
 }
