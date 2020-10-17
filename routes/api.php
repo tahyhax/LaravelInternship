b771de60ca36d0035c7c64c3711cf1846d922632
tahyhax <dimon4i4ek@ukr.net>
2020-10-17 15:45:31 +0300

    Lesson-3 include  sanctum auth

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 // TODO 'middleware' => ['auth:api'] говорит что нет auth token
-Route::group(['as' => 'api.','namespace' => '\App\Http\Controllers\Api'], function () {
+Route::group(['as' => 'api.','namespace' => '\App\Http\Controllers\Api' ], function () {
     Route::apiResource('categories',    CategoryController::class);
 });
