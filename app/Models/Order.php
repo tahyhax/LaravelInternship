baf483b5752dbe0f75d8f8d0cb13ac3259e995d5
tahyhax <dimon4i4ek@ukr.net>
2020-10-19 18:32:04 +0300

    Lesson-3  add  order  api  - model  - resource  - requests

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'address', 'status', 'payment_methods_id', 'user_id',
+    ];
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function paymentMethod()
+    {
+        return $this->belongsTo(PaymentMethod::class,'payment_methods_id');
+    }
+}
