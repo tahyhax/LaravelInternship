125e91f46836581deede0ec461e918797e912d5f
tahyhax <dimon4i4ek@ukr.net>
2020-10-21 01:37:59 +0300

    Lesson-3 small refactor  and  static  analyze

--- app/Http/Controllers/Front/MainController.php
+++ app/Http/Controllers/Front/MainController.php
@@ -2,25 +2,23 @@
 
 namespace App\Http\Controllers\Front;
 
-
 use App\Http\Controllers\Controller;
-
 use App\Models\Product;
 
 class MainController extends Controller
 {
+    protected $perPage = 12;
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    const MAIN_PRODUCTS_COUNT = 12;
-
     public function __invoke()
     {
         //TODO  сделать repository для  работы с базой. не писать сами запросы  в  контроллере
-        $products = Product::query()->orderByDesc('id')->paginate(self::MAIN_PRODUCTS_COUNT);
+        $products = Product::query()->orderByDesc('id')->paginate($this->perPage);
 
-        return view('front.main.index')->with(compact('products'));
+        return view('front.main.index')->with(compact(['products']));
     }
 }
