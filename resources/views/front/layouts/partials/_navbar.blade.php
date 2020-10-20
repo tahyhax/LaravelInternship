1e915bd58b52ba6a1e20e9df7c7c514957a71505
tahyhax <dimon4i4ek@ukr.net>
2020-10-21 01:40:44 +0300

    Lesson-3 show  suser avatar

--- resources/views/front/layouts/partials/_navbar.blade.php
+++ resources/views/front/layouts/partials/_navbar.blade.php
@@ -1,12 +1,16 @@
+@php
+ $customer = Auth::user();
+@endphp
 <ul class="navbar-nav ">
     <li class="nav-item text-nowrap">
         @guest
             <a class="btn btn-outline-primary" href="{{ route('login') }}">Sign in</a>
         @else
+            <img class="rounded" src="{{$customer->avatar}}" alt="$customer->name" width="50px" height="50px" >
             <div class="btn-group">
                 <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
-                    {{ Auth::user()->name }}
+                    {{ $customer->name }}
                 </button>
                 <div class="dropdown-menu dropdown-menu-right">
                     <a class="dropdown-item" href="{{ route('cabinet.main.index') }}">
