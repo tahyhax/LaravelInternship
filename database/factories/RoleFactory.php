fda37c07e4341bb1d391db170250ce4dce25a61b
tahyhax <dimon4i4ek@ukr.net>
2020-10-22 00:40:58 +0300

    Lesson-3 add  role  factory

---
+++ database/factories/RoleFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Role;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class RoleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Role::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->unique()->randomElement([
+            'Admin',
+            'SuperAdmin',
+            'Manager',
+            'User',
+            'Customer',
+         ]);
+        $slug = Str::slug($name);
+
+        return [
+            'name' => $name,
+            'slug' => $slug,
+            'description' => $this->faker->sentence
+        ];
+    }
+}
