76f7bdc8fff726c5e2bd79f846afaaaff66cfab2
tahyhax <dimon4i4ek@ukr.net>
2020-10-23 02:33:40 +0300

    SHP-1 fix user  factory remove  role_id

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,29 +24,14 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        $name = $this->faker->randomElement([
-            'Admin',
-            'Manager',
-            'ManagerOrder',
-        ]);
-
-
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'role_id'=> $this->getRoleId($name)
         ];
     }
-
-    private function getRoleId($name)
-    {
-        $role = Role::query()->select('id')->where('slug', Str::slug($name))->first();
-        return $role ?? 1;
-    }
-
 
 
 }
