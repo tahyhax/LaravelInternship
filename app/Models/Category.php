665448ba2954da4e67de798a5ae3f51addd4467b
tahyhax <dimon4i4ek@ukr.net>
2020-10-23 03:41:09 +0300

    SHP-1  static analyze

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -20,9 +20,7 @@ class Category extends Model
 
     public function children()
     {
-
         return $this->hasMany(Category::class, 'parent_id', 'id');
-
     }
 
     public function products()
