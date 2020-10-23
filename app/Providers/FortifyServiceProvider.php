46f99ddd84e9ff11616b519e55d20f090049d60d
tahyhax <dimon4i4ek@ukr.net>
2020-10-23 16:50:08 +0300

    SHP-1  comment  not used  code

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -38,12 +38,12 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
-        Fortify::authenticateUsing(function (LoginRequest $request) {
-            $user = User::query()->where('email', $request->email)->first();
-            if ($user && Hash::check($request->password, $user->password)) {
-                return $user;
-            }
-        });
+//        Fortify::authenticateUsing(function (LoginRequest $request) {
+//            $user = User::query()->where('email', $request->email)->first();
+//            if ($user && Hash::check($request->password, $user->password)) {
+//                return $user;
+//            }
+//        });
 
         Fortify::loginView(function () {
             return view('auth.login');
