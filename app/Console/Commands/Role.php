eb549d88c806e05f443f1d7b9a97916ddd6a6108
tahyhax <dimon4i4ek@ukr.net>
2020-10-27 01:43:54 +0200

    SHP-1 add  comand  role ,  now  you  can  edit user  role via  console

---
+++ app/Console/Commands/Role.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Models\Role as UserRole;
+use Illuminate\Console\Command;
+
+class Role extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'role:set
+                            {email : user email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set user role by email';
+
+//    /**
+//     * Create a new command instance.
+//     *
+//     * @return void
+//     */
+//    public function __construct()
+//    {
+//        parent::__construct();
+//    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+
+        $user = User::query()->where('email', $email)->first();
+
+        if (!$user) {
+            $this->error('No user with this email');
+            return 0;
+        }
+
+        $roles = UserRole::query()->get();
+
+        $rolesName = $roles->pluck(['name'])->toArray();
+
+        $choice = $this->choice('select role', $rolesName, 0, null, true);
+
+        $rolesIds = $roles->whereIn('name', $choice)->pluck('id');
+
+        if ($this->confirm("You really have update  user $email")) {
+            if ($user->roles()->sync($rolesIds)) {
+
+                $this->info('Roles have been updated');
+            }
+        }
+    }
+}
