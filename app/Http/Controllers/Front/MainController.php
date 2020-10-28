a68f81193a902d2ed6c80df612fa93c4e1603309
tahyhax <dimon4i4ek@ukr.net>
2020-10-28 23:18:24 +0200

    SHP-1 get  images  by 5  last  products

--- app/Http/Controllers/Front/MainController.php
+++ app/Http/Controllers/Front/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\ProductFilters;
+use App\Models\Image;
 use App\Models\Product;
 use App\Traits\Filterable;
 
@@ -25,6 +26,8 @@ class MainController extends Controller
         //TODO  сделать repository для  работы с базой. не писать сами запросы  в  контроллере
         $products = Product::query()->filter($filtes)->orderByDesc('id')->paginate($this->perPage);
 
-        return view('front.main.index')->with(compact(['products']));
+        //TODO   как правильно сделать !!
+        $slideImages = Image::query()->with('product')->latest('id')->limit(5)->get();
+        return view('front.main.index')->with(compact(['products', 'slideImages']));
     }
 }
