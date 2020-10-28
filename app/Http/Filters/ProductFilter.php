ff594c4c8cd3e70e645d7d26002264fdbf5a7ed0
tahyhax <dimon4i4ek@ukr.net>
2020-10-28 15:18:50 +0200

    SHP-1 add add  product filter

---
+++ app/Http/Filters/ProductFilter.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Filters;
+
+use Illuminate\Database\Query\Builder;
+
+class ProductFilter extends QueryFilter
+{
+
+    /**
+     * @param string $categories
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function categories(string $categories)
+    {
+        return $this->builder->whereHas('categories', function ($query) use ($categories) {
+
+            /* @var Builder $query*/
+            $query->whereIn('slug', $this->paramToArray($categories));
+        });
+
+
+    }
+
+    /**
+     * @param string $brands
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function brands(string $brands)
+    {
+        return $this->builder->whereHas('brand', function($query) use ($brands) {
+
+            /* @var Builder $query*/
+            $query->whereIn('slug', $this->paramToArray($brands));
+        });
+    }
+
+    /**
+     * @param string $sort
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function sort(string $sort)
+    {
+        return $this->builder->orderBy($sort, 'ASC');
+    }
+
+    /**
+     * @param string $price
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function price(string $price)
+    {
+        return $this->builder->whereBetween('price', $this->paramBetweenToArray($price));
+    }
+
+    /**
+     * @param $keyword
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function search($keyword)
+    {
+        return $this->builder->where('name', 'like', '%'.$keyword.'%');
+    }
+
+}
