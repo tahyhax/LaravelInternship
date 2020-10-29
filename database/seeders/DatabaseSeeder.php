736a1c6987d387c2ac7003379facff3eb6d492e1
tahyhax <dimon4i4ek@ukr.net>
2020-10-29 12:22:17 +0200

    SHP-1 add post seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             BrandSeeder::class,
-            CategorySeeder::class
+            CategorySeeder::class,
+            PostSeeder::class
         ]);
 
     }
