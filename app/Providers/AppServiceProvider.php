546b4731a16f09397f738eb3bf710f8f7dc64f31
tahyhax <dimon4i4ek@ukr.net>
2020-11-04 00:50:16 +0200

    SHP-2  registerBindings repository  cart

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\Cart\Repositories\CartRepositoryInterface;
+use App\Services\Cart\Repositories\CartSessionRepository;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use App\Providers\Views\BladeStatements;
@@ -9,6 +11,7 @@ use App\Providers\Views\BladeStatements;
 class AppServiceProvider extends ServiceProvider
 {
     use BladeStatements;
+
     /**
      * Register any application services.
      *
@@ -27,9 +30,19 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->bootBladeStatements();
+        $this->registerBindings();
+        $this->registerPagination();
 
+    }
+
+    private function registerBindings()
+    {
+        $this->app->bind(CartRepositoryInterface::class, CartSessionRepository::class);
+    }
+
+    private function registerPagination()
+    {
         Paginator::defaultView('front.blocks.pagination');
-
         Paginator::defaultSimpleView('front.blocks.pagination');
     }
 }
