dd12b350fea48a03851e3603cae0429a07e2b60d
tahyhax <dimon4i4ek@ukr.net>
2020-11-03 17:10:24 +0200

    SHP-2  show  old  value  after  validation

--- resources/views/front/checkout/partials/from/fields.blade.php
+++ resources/views/front/checkout/partials/from/fields.blade.php
@@ -1,7 +1,8 @@
 <div class="form-row">
     <div class="form-group col-6">
         <label for="last_name">First name</label>
-        <input type="text" name="last_name" class="form-control" id="last_name" required>
+        <input type="text" name="last_name" class="form-control" id="last_name"
+               value="{{old('last_name')}}" required>
         @error('last_name')
         <div class="invalid-feedback">
             {{$message}}
@@ -11,7 +12,8 @@
 
     <div class="form-group col-6">
         <label for="first_name">Last name</label>
-        <input type="text" name="first_name" class="form-control" id="first_name" required>
+        <input type="text" name="first_name" class="form-control" id="first_name"
+               value="{{old('first_name')}}" required>
         @error('first_name')
         <div class="invalid-feedback">
             {{$message}}
@@ -24,7 +26,7 @@
     <div class="form-group col-6">
         <label for="email">Email</label>
         <input type="email" name="email" class="form-control" id="email"
-               placeholder="name@example.com" required>
+               placeholder="name@example.com" value="{{old('email')}}" required>
         @error('email')
         <div class="invalid-feedback">
             {{$message}}
@@ -34,7 +36,8 @@
 
     <div class="form-group col-6">
         <label for="phone">Phone</label>
-        <input type="text" name="phone" class="form-control" id="phone" placeholder="+38095-121-21-23" required>
+        <input type="text" name="phone" class="form-control" id="phone" placeholder="+38095-121-21-23"
+               value="{{old('phone')}}" required>
         @error('phone')
         <div class="invalid-feedback">
             {{$message}}
@@ -45,7 +48,8 @@
 <div class="form-row">
     <div class="form-group col-9">
         <label for="address">Address</label>
-        <input type="text" name="address" class="form-control" id="address" required>
+        <input type="text" name="address" class="form-control" id="address"
+               value="{{old('address')}}"  required>
         @error('address')
         <div class="invalid-feedback">
             {{$message}}
