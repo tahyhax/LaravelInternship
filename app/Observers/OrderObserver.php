9de5ea765bee1961f5792e4085baea1dd1c882c8
tahyhax <dimon4i4ek@ukr.net>
2020-11-08 01:46:41 +0200

    SHP-2 create  observer

---
+++ app/Observers/OrderObserver.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Order;
+use App\Notifications\OrderCreatedNotification;
+use App\Notifications\OrderUpdatedNotification;
+use Illuminate\Support\Facades\Notification;
+
+class OrderObserver
+{
+    /**
+     * Handle the order "created" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function created(Order $order)
+    {
+        Notification::route('mail', $order->email)->notify(new OrderCreatedNotification($order));
+    }
+
+    /**
+     * Handle the order "updated" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function updated(Order $order)
+    {
+        Notification::route('mail', $order->email)->notify(new OrderUpdatedNotification($order));
+    }
+
+}
