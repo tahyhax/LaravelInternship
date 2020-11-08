a20085086b2f9ef9878876732dddb31cb25c8509
tahyhax <dimon4i4ek@ukr.net>
2020-11-09 00:06:05 +0200

    SHP-2 add  event  to remove old file  from store

---
+++ app/Events/ImagesEvent.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Image;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ImagesEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $image;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ app/Listeners/ImagesListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ImagesEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Storage;
+
+class ImagesListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  ImagesEvent  $event
+     * @return void
+     */
+    public function handle(ImagesEvent $event): void
+    {
+        //TODO  непонтянтно почуму не работает если сразу передать массив на удаление
+        foreach ($event->image as $item) {
+            Storage::delete($item->storage_link . '/' . $item->name);
+        }
+
+    }
+}
