99032fe51c22b75cdb724ac372af3addfa02bf3b
tahyhax <dimon4i4ek@ukr.net>
2020-11-09 20:48:35 +0200

    SHP-2 fix  markdown mesasge template

--- resources/views/front/mails/orders/order-created.blade.php
+++ resources/views/front/mails/orders/order-created.blade.php
@@ -1,17 +1,17 @@
 @component('mail::message')
-    # Order: **{{$order->slug}}** status ``{{$order->status}}``
+# Order: **{{$order->slug}}** status `New`
 
-    @component('mail::table')
-        Name  | Price  | Qty  | Total
-        --- |---|--- |---
-        @foreach($order->products as $item)
-            {{$item->name}}    | {{$item->price}}  |   {{$item->price}}  | {{$item->price * $item->orderItem->qty}}
-        @endforeach
-    @endcomponent
+@component('mail::table')
+| Name       |Price | Qty   | Total|
+|:---------- |:-----:|:---: |:-----|
+@foreach($order->products as $item)
+| {{$item->name}} | {{$item->price}} | {{$item->price}} | {{$item->price * $item->orderItem->qty}}|
+@endforeach
+@endcomponent
 
-    @component('mail::footer')
-        Thanks,<br>
-        [{{ config('app.name') }}]
+@component('mail::footer')
+Thanks,<br>
+[{{ config('app.name') }}]
 
-    @endcomponent
+@endcomponent
 @endcomponent
