e08d68ef5413d7286d0fb7c601ddbb3f65d690d8
tahyhax <dimon4i4ek@ukr.net>
2020-11-09 20:48:14 +0200

    SHP-2 register event  order created

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Events\ImagesEvent;
+use App\Events\OrderCreatedEvent;
 use App\Listeners\ImagesListener;
+use App\Listeners\OrderCreatedListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -22,6 +24,9 @@ class EventServiceProvider extends ServiceProvider
         ],
         ImagesEvent::class => [
             ImagesListener::class
+        ],
+        OrderCreatedEvent::class => [
+            OrderCreatedListener::class
         ]
     ];
 
