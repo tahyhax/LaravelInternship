07b6d80bb208976ebcec850c36be53bc39150375
tahyhax <dimon4i4ek@ukr.net>
2020-11-09 20:47:53 +0200

    SHP-2 add  event  order created

---
+++ app/Listeners/OrderCreatedListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Notifications\OrderCreatedNotification;
+use Illuminate\Support\Facades\Notification;
+
+class OrderCreatedListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        Notification::route('mail', $event->order->email)->notify(new OrderCreatedNotification($event->order));
+    }
+}
