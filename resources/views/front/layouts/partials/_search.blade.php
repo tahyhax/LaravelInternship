c0979fce170e180b06735760c0864e871aae9f19
tahyhax <dimon4i4ek@ukr.net>
2020-11-09 21:36:22 +0200

    SHP-2 add  filter to main

--- resources/views/front/layouts/partials/_search.blade.php
+++ resources/views/front/layouts/partials/_search.blade.php
@@ -1,1 +1,4 @@
-<input class="form-control  form-control" type="text" placeholder="Search" aria-label="Search">
+<form method="GET" class="w-100">
+    @csrf
+    <input class="form-control  form-control" name="search" type="text" placeholder="Search" aria-label="Search">
+</form>
