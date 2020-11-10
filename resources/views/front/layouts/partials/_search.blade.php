1b993d134add22722884df20542440b2f08dacbf
tahyhax <dimon4i4ek@ukr.net>
2020-11-11 01:32:22 +0200

    SHP-2 set current search  value in to input

--- resources/views/front/layouts/partials/_search.blade.php
+++ resources/views/front/layouts/partials/_search.blade.php
@@ -1,4 +1,4 @@
 <form method="GET" class="w-100">
     {{--@csrf--}}
-    <input class="form-control  form-control" name="search" type="text" placeholder="Search" aria-label="Search">
+    <input class="form-control  form-control" name="search" type="text" placeholder="Search" aria-label="Search" value="{{request()->get('search')}}">
 </form>
