14f527b24409b7d5e5d6db83d069d7ac5cc34c68
tahyhax <dimon4i4ek@ukr.net>
2020-11-14 00:40:34 +0200

    SHP-2 add  products  index

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -3,10 +3,33 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\ProductFilters;
 use App\Models\Product;
 
 class ProductController extends Controller
 {
+    /**
+     * @var int
+     */
+    protected $perPage = 10;
+
+    /**
+     * @param ProductFilters $filters
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(ProductFilters $filters)
+    {
+
+        return view('front.products.index')->with(
+            [
+                'products' =>
+                    Product::query()->with('imagesMain')
+                        ->filter($filters)
+                        ->orderByDesc('id')
+                        ->paginate($this->perPage),
+            ]
+        );
+    }
 
     /**
      * Display the specified resource.
