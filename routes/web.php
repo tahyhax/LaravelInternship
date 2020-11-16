6216217617c7a2d62dbeeaf1dbf2c79dded76e33
tahyhax <dimon4i4ek@ukr.net>
2020-11-16 20:45:11 +0200

    SHP-2 refactor web routes

--- routes/web.php
+++ routes/web.php
@@ -15,30 +15,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Http\Controllers\Front\MainController::class)->name('main.index');
 
-Route::get('/category/{category:slug}', [\App\Http\Controllers\Front\CategoryController::class, 'show'])
+Route::get('/category/{category}', [\App\Http\Controllers\Front\CategoryController::class, 'show'])
     ->name('category.show');
 
-Route::get('/product/{product:slug}', [\App\Http\Controllers\Front\ProductController::class, 'show'])
+Route::get('products', [\App\Http\Controllers\Front\ProductController::class, 'index'])
+    ->name('products.index');
+
+Route::get('/products/{product}', [\App\Http\Controllers\Front\ProductController::class, 'show'])
     ->name('products.show');
 
+Route::get('/posts', [\App\Http\Controllers\Front\PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post}', [\App\Http\Controllers\Front\PostController::class, 'show'])->name('posts.show');
 
 Route::get('/cart', \App\Http\Livewire\Cart::class)->name('cart');
-Route::get('/checkout', [\App\Http\Controllers\Front\CheckoutController::class, 'show'])->name('checkout.show');
-Route::post('/checkout/ordering', [\App\Http\Controllers\Front\CheckoutController::class, 'ordering'])->name('checkout.ordering');
+Route::get('/checkout', [\App\Http\Controllers\Front\CheckoutController::class, 'show'])
+    ->name('checkout.show');
 
+Route::post('/checkout/ordering', [\App\Http\Controllers\Front\CheckoutController::class, 'ordering'])
+    ->name('checkout.ordering');
 
-//Route::resource('posts', \App\Http\Controllers\PostController::class);
-
-//Route::get('posts', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
-//Route::get('posts/{post}/show', [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
-
-//Route::middleware('auth')->group(function() {
-//    Route::get('posts/create', [\App\Http\Controllers\PostController::class, 'create'])->name('posts.create');
-//    Route::get('posts/{post}/edit', [\App\Http\Controllers\PostController::class, 'edit'])->name('posts.edit');
-//    Route::delete('posts/{post}', [\App\Http\Controllers\PostController::class, 'destroy'])->name('posts.destroy');
-//    Route::post('posts', [\App\Http\Controllers\PostController::class, 'store'])->name('posts.store');
-//    Route::put('posts/{post}', [\App\Http\Controllers\PostController::class, 'update'])->name('posts.update');
-//});
 Route::group(['as' => 'cabinet.', 'prefix' => 'cabinet', 'middleware' => ['auth', 'role:customer']],
     function () {
         Route::get('/', \App\Http\Controllers\Cabinet\MainController::class)
@@ -52,14 +47,3 @@ Route::group(['as' => 'cabinet.', 'prefix' => 'cabinet', 'middleware' => ['auth'
         Route::put('/customer-info/{customer}/update', [\App\Http\Controllers\Cabinet\CustomerInfoController::class, 'update'])
             ->name('customer-info.update');
     });
-
-
-//Route::name('admin.')
-////    ->namespace('\App\Http\Controllers\Dashboard')
-//    ->prefix('admin')
-//    ->group(function() {
-//        Route::get('/', \App\Http\Controllers\Dashboard\MainController::class)
-//        ->name('main.index');
-//        Route::get('/users', [\App\Http\Controllers\Dashboard\UserController::class, 'index'])
-//            ->name('users.index');
-//});
