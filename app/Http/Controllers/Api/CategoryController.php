94f19ef697dad37ea30275aab1333886a45009f7
tahyhax <dimon4i4ek@ukr.net>
2020-11-16 23:41:37 +0200

    SHP-2 return category children

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -60,7 +60,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        $category->load('images');
+        $category->load(['images', 'children']);
         return new CategoryResource($category);
     }
 
