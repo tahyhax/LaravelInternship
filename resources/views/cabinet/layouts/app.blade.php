1e0d225a29a8d27deb082118d491bdb90d34bf53
tahyhax <dimon4i4ek@ukr.net>
2020-11-18 00:49:33 +0200

    SHP-2 fix cabinet styles

--- resources/views/cabinet/layouts/app.blade.php
+++ resources/views/cabinet/layouts/app.blade.php
@@ -9,10 +9,12 @@
         @include('cabinet.layouts.partials._navbar')
 
         <div class="container-fluid">
-            <div class="row">
+            <div class="row h-100">
                 @include('cabinet.layouts.partials._sidebar-left')
                 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-                    @yield('content')
+                    <div class="mt-2">
+                        @yield('content')
+                    </div>
                 </main>
             </div>
         </div>
