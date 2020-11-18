dcbe02a199e03d55ec9d606cfa7391a1cb304108
tahyhax <dimon4i4ek@ukr.net>
2020-11-18 13:39:42 +0200

    SHP-2 add in menu lint to orders

--- resources/views/cabinet/layouts/partials/_sidebar-left.blade.php
+++ resources/views/cabinet/layouts/partials/_sidebar-left.blade.php
@@ -3,7 +3,7 @@
         <i class="fa fa-user mr-1"></i>
         {{__('Personal Info')}}
     </a>
-    <a class="list-group-item list-group-item-action" href="{{route('cabinet.customer-info.show')}}">
+    <a class="list-group-item list-group-item-action" href="{{route('cabinet.orders.index')}}">
         <i class="fa  fa-shopping-bag mr-1"></i>
         {{__('Orders')}}
     </a>
