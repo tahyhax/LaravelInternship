ae14d27dc04af320dfe315c86a228c403edad071
tahyhax <dimon4i4ek@ukr.net>
2020-11-20 00:31:39 +0200

    SHP-2  small refactor , use  children group

--- routes/api.php
+++ routes/api.php
@@ -18,31 +18,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['as' => 'api.'], function () {
+Route::group(['as' => 'api.', 'namespace' => '\App\Http\Controllers\Api'], function () {
+
     Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
-});
+
+    Route::group(['middleware' => ['auth:sanctum', 'verify-route-access']], function () {
+
+        Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+        Route::put('orders/update-status/{order}', [\App\Http\Controllers\Api\OrderController::class, 'updateStatus']);
+
+        Route::post('categories/{category}', [\App\Http\Controllers\Api\CategoryController::class, 'update']);
+        Route::apiResource('categories', CategoryController::class)->except('update');
+
+        Route::post('products/{product}', [\App\Http\Controllers\Api\ProductController::class, 'update']);
+        Route::apiResource('products', ProductController::class)->except('update');
 
 
-Route::group(['as' => 'api.', 'namespace' => '\App\Http\Controllers\Api',
-    'middleware' => ['auth:sanctum']], function () {//, 'verify.route.access'
+        Route::apiResources([
+            'roles' => RoleController::class,
+            'permissions' => PermissionController::class,
+            'brands' => BrandController::class,
+            'posts' => PostController::class,
+            'payment-methods' => PaymentMethodController::class,
+            'orders' => OrderController::class,
+        ]);
 
-    Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
-    Route::put('orders/update-status/{order}', [\App\Http\Controllers\Api\OrderController::class, 'updateStatus']);
-
-    Route::post('categories/{category}', [\App\Http\Controllers\Api\CategoryController::class, 'update']);
-    Route::apiResource('categories', CategoryController::class)->except('update');
-
-    Route::post('products/{product}', [\App\Http\Controllers\Api\ProductController::class, 'update']);
-    Route::apiResource('products', ProductController::class)->except('update');
-
-
-    Route::apiResources([
-        'roles' => RoleController::class,
-        'permissions' => PermissionController::class,
-        'brands' => BrandController::class,
-        'posts' => PostController::class,
-        'payment-methods' => PaymentMethodController::class,
-        'orders' => OrderController::class,
-    ]);
+    });
 
 });
