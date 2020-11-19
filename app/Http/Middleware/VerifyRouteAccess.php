399101f7d1c8b7b77406cb7aa6f60542d3e2c651
tahyhax <dimon4i4ek@ukr.net>
2020-11-20 00:32:34 +0200

    SHP-2 test  version , but need  create  normal response for api

--- app/Http/Middleware/VerifyRouteAccess.php
+++ app/Http/Middleware/VerifyRouteAccess.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class VerifyRouteAccess
 {
@@ -16,12 +17,16 @@ class VerifyRouteAccess
      */
     public function handle(Request $request, Closure $next)
     {
-
-        if ($request->user()->cannot('access-route', $request->route()->getName())) {
-            throw new AuthorizationException(
-                __('You are not authorized to perform this action')
-            );
-        }
+        //TODO  как вернуть нормльную ошибку для  api
+        abort_if(
+            $request->user()->cannot('access-route', $request->route()->getName()),
+            Response::HTTP_FORBIDDEN, '403 Forbidden'
+        );
+//        if ($request->user()->cannot('access-route', $request->route()->getName())) {
+//            throw new AuthorizationException(
+//                __('You are not authorized to perform this action')
+//            );
+//        }
 
         return $next($request);
     }
