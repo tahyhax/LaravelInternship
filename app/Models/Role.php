1f3ad1b59ec12c07ba34033f598669e035917baa
tahyhax <dimon4i4ek@ukr.net>
2020-11-19 02:44:16 +0200

    SHP-2  add  param access  to pivot table

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -41,6 +41,7 @@ class Role extends Model
 
     public function permissions()
     {
-        return $this->belongsToMany(Permission::class);
+        return $this->belongsToMany(Permission::class)
+            ->withPivot('access')->as('permissionRole');
     }
 }
