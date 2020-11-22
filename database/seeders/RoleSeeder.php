99361e618b743d4109782baa8d8a5309f523badd
tahyhax <dimon4i4ek@ukr.net>
2020-11-22 13:01:01 +0200

    SHP-2 edit  description

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -18,7 +18,7 @@ class RoleSeeder extends Seeder
             [
                 'name' => 'SuperAdmin',
                 'slug' => 'super-admin',
-                'description' => 'this is  man like  god'
+                'description' => 'this is  man like  jambo'
             ],
             [
                 'name' => 'customer',
